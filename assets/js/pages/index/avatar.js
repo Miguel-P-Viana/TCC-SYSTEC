ac523f076d7bdb1ab90abde7fb554250dcca5b6e
Miguel-P-Viana <migueldsistemas@gmail.com>
2026-02-12 16:03:05 -0300

    Fisrt Commit

---
+++ assets/js/pages/index/avatar.js
@@ -0,0 +1,12 @@
+
+function Avatar() {
+
+    const icon_avatar = document.getElementById('avatar')
+
+    
+
+    const main = document.getElementsByTagName('main')[0]
+
+   main.appendChild(conta)
+    
+}
